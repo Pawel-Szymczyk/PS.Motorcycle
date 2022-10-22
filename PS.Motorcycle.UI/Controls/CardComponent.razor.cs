11932b3f0c686d36c46809a000dd853cc87027e4
Pawel <szymczyk.pawel@yahoo.co.uk>
2022-10-22 01:53:44 +0100

    Work on User Portal, create cards layout, add styling , add sass

---
+++ PS.Motorcycle.UI/Controls/CardComponent.razor.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PS.Motorcycle.UserPortal.Controls
+{
+    public partial class CardComponent : ComponentBase
+    {
+    }
+}
