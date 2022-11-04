b5c1ef1fd743f1a73091f41bbd20442695ec613e
Pawel <szymczyk.pawel@yahoo.co.uk>
2022-11-04 00:48:10 +0000

    Work on search motorcycle body type carousel

---
+++ PS.Motorcycle.Common/Controls/BodyTypeCaruselComponent.razor.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PS.Motorcycle.Common.Controls
+{
+    public partial class BodyTypeCaruselComponent : ComponentBase
+    {
+
+        [Inject]
+        public IJSRuntime JS { get; set; } = default!;
+
+        private bool firstRender = true;
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            if (firstRender)
+            {
+                //if (!this.Images.Count.Equals(0))
+                //{
+                //    await this.JS.InvokeAsync<object>("initializeSlides");
+                //}
+
+                await this.JS.InvokeAsync<object>("initializeSearchCarousel");
+
+                this.firstRender = false;
+            }
+        }
+    }
+}
